ece9248ee985a21a3ea4317099136d8ba5d30df1
fanstats <31288556+fanstats@users.noreply.github.com>
2024-11-18 21:22:05 -0500

    update kmeans init

--- R-codes/initial.R
+++ R-codes/initial.R
@@ -1,20 +1,33 @@
 random.init = function(p,q,K){
   # Purpose: randomly initialize M,L,D and set pk = 1/K
-  M = D = SD <- matrix(NA,p,K)
-  L <- array(NA,c(p,q,K))
-  
-  for (k in 1:K) {
-    M[,k] <- rnorm(p)
-    Lk <- matrix(rnorm(p*q),p,q)
-    D[,k] <- runif(p,0.2,0.8)
-    Gamma <- crossprod(Lk,Lk/D[,k])
-    eig <- eigen(Gamma)$vectors
-    L[,,k] <- Lk%*%eig
+  if(q>0){
+    M = D <- matrix(NA,p,K)
+    L <- array(NA,c(p,q,K))
+    
+    for (k in 1:K) {
+      M[,k] <- rnorm(p)
+      Lk <- matrix(rnorm(p*q),p,q)
+      D[,k] <- runif(p,0.2,0.8)
+      Gamma <- crossprod(Lk,Lk/D[,k])
+      eig <- eigen(Gamma)$vectors
+      L[,,k] <- Lk%*%eig
+    }
+    prob = rep(1/K,K)
+    
+    return(list(M = M,L = L,D = D,prob = prob))
+    
+  }else{
+    M <- matrix(NA,p,K)
+    S <- array(NA,c(p,p,K))
+    for (k in 1:K) {
+      M[,k] <- rnorm(p)
+      A <- matrix(rnorm(p^2), p, p) 
+      S[,,k] <- t(A) %*% A
+    }
+    prob = rep(1/K,K)
+    
+    return(list(M = M, S = S, prob = prob))
   }
-  
-  prob = rep(1/K,K)
-  
-  return(list(M = M,L = L,D = D,prob = prob))
 }
 
 random.init.error = function(p,q,K){
@@ -39,3 +52,29 @@ random.init.error = function(p,q,K){
   
   return(list(M = M,L = L,U = U,tau = tau,alpha = alpha,prob = prob))
 }
+
+# data-driven initial
+data.init = function(X, prop, p, q, K,seed){
+  options(warn = -1)
+  
+  n = nrow(X)
+  size = round(prop*n)
+  
+  set.seed(seed)
+  x = X[sample.int(n,size = size,replace = F),]
+  cl.kmeans = kmeans(x,centers = K,iter.max = 1,nstart = 1)$cluster
+  
+  M = D = matrix(NA,p,K)
+  L = array(NA,c(p,q,K))
+  for (k in 1:K) {
+    x.k = x[cl.kmeans==k,]
+    res.k = fad::fad(x.k,q)
+    M[,k] = colMeans(x.k)
+    D[,k] = res.k$sd^2*res.k$uniquenesses
+    L[,,k] = res.k$sd*res.k$loadings
+  }
+  prob = rep(1/K,K)
+  
+  return(list(M = M,L = L,D = D,prob = prob))
+}
+
